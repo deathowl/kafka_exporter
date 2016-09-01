6ad0df5faac936cd8ee78f50de62762166959ab1
deathowl <bcsergo@hortonworks.com>
2016-09-01 15:07:51 +0200

    refactor a bit, also partition count per topic

--- kafka.go
+++ kafka.go
@@ -12,13 +12,7 @@ import (
 type kafkaCollector struct {
 	upIndicator *prometheus.Desc
 	topicsCount *prometheus.Desc
-	metrics     map[string]kafkaMetric
-}
-type kafkaMetric struct {
-	desc          *prometheus.Desc
-	extract       func(string) float64
-	extractLabels func(s string) []string
-	valType       prometheus.ValueType
+	partitionsCount * prometheus.Desc
 }
 func init() {
 	prometheus.MustRegister(NewKafkaCollector())
@@ -37,22 +31,14 @@ func NewKafkaCollector() *kafkaCollector {
 	return &kafkaCollector{
 		upIndicator: prometheus.NewDesc("kafka_up", "Exporter successful", nil, nil),
 		topicsCount: prometheus.NewDesc("kafka_topics", "Count of topics", nil, nil),
-		metrics: map[string]kafkaMetric{
-			"kafka_topics": {
-				desc:    prometheus.NewDesc("kafka_placeholder", "Placeholder metric", nil, nil),
-				extract: func(topics string) float64 { return parseFloatOrZero(topics) },
-				valType: prometheus.GaugeValue,
-			},
-		},
-
+		partitionsCount: prometheus.NewDesc("kafka_partitions", "Count of partitions per topic",  []string{"topic"}, nil),
 	}
 }
 
 func (c *kafkaCollector) Describe(ch chan<- *prometheus.Desc) {
-	log.Debugf("Sending %d metrics descriptions", len(c.metrics))
-	for _, i := range c.metrics {
-		ch <- i.desc
-	}
+	ch <- c.upIndicator
+	ch <- c.topicsCount
+	ch <- c.partitionsCount
 }
 
 func (c *kafkaCollector) Collect(ch chan<- prometheus.Metric) {
@@ -61,17 +47,22 @@ func (c *kafkaCollector) Collect(ch chan<- prometheus.Metric) {
 	var topics []string
         var kafkaConsumer sarama.Consumer
 	var err error
+	var partitions []int32
         kafkaConsumer, err = sarama.NewConsumer(strings.Split(*kafkaAddr, ","), nil)
 	if err != nil {
 		log.Fatalln("Failed to start Sarama consumer:", err)
 	}
 	
+	ch <- prometheus.MustNewConstMetric(c.upIndicator, prometheus.GaugeValue, 1)
 	topics, err = kafkaConsumer.Topics()
 	if err !=nil {
 		log.Error("Failed to get list of topics")
 		ch <- prometheus.MustNewConstMetric(c.upIndicator, prometheus.GaugeValue, 0)
 	}
 	ch <- prometheus.MustNewConstMetric(c.topicsCount, prometheus.GaugeValue, float64(len(topics)))
-	log.Info(topics)
-	ch <- prometheus.MustNewConstMetric(c.upIndicator, prometheus.GaugeValue, 1)
+	for _, topic := range topics{
+		partitions,_ = kafkaConsumer.Partitions(topic)
+		ch <- prometheus.MustNewConstMetric(c.partitionsCount, prometheus.GaugeValue,float64(len(partitions)), topic)
+
+	}
 }
